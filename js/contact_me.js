83f659b8a051514cb85d7531e03f4d31663f7cca
zunairahshahzad <111823557+zunairahshahzad@users.noreply.github.com>
2022-09-18 10:04:57 -0400

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,17 +1,17 @@
 $(function() {
 
     $("input,textarea").jqBootstrapValidation({
-        preventSubmit: false,
+        preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
         },
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
-            var name = $("input#name").val(); 
-            var email = $("input#email").val(); 
-            var phone = $("input#phone").val(); 
-            var message = $("textarea#message").val(); 
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var phone = $("input#phone").val();
+            var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
@@ -26,7 +26,7 @@ $(function() {
                     email: email,
                     message: message
                 },
-                cache: true,
+                cache: false,
                 success: function() {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
